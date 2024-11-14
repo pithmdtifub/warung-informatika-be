5a9ecd3eabdb8552d1bfdebfd0640e10e028be07
atha3417 <atha.3417@gmail.com>
2024-11-14 17:46:05 +0700

    chore: arrange column

--- models/user.go
+++ models/user.go
@@ -2,7 +2,7 @@ package models
 
 type User struct {
 	ID       uint   `json:"id" gorm:"primaryKey"`
-	Role     string `json:"role" gorm:"type:role;not null;default:'Admin'"`
 	Username string `json:"username" form:"username" validate:"required"`
+	Role     string `json:"role" gorm:"type:role;not null;default:'Admin'"`
 	Password string `json:"password" form:"password" validate:"required"`
 }
