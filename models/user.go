65f649e7636a8e7656cbb3698f61e8f2a5437ca3
atha3417 <atha.3417@gmail.com>
2024-11-14 17:36:59 +0700

    chore: add role to user

--- models/user.go
+++ models/user.go
@@ -2,6 +2,7 @@ package models
 
 type User struct {
 	ID       uint   `json:"id" gorm:"primaryKey"`
+	Role     string `json:"role" gorm:"type:role;not null;default:'Admin'"`
 	Username string `json:"username" form:"username" validate:"required"`
 	Password string `json:"password" form:"password" validate:"required"`
 }
