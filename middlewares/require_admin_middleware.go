0421396ddf18c93f65bc00e78bbab84a9ac18335
atha3417 <atha.3417@gmail.com>
2024-11-14 18:09:44 +0700

    feat: require admin middleware

--- middlewares/require_admin_middleware.go
+++ middlewares/require_admin_middleware.go
@@ -1,1 +1,35 @@
 package middlewares
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/golang-jwt/jwt/v5"
+	"os"
+	"strings"
+)
+
+func RequireAdmin(c *fiber.Ctx) error {
+	key := os.Getenv("JWT_SECRET")
+	var jwtKey = []byte(key)
+
+	tokenString := c.Get("Authorization")
+	tokenArray := strings.Split(tokenString, " ")
+	if len(tokenArray) == 1 {
+		tokenString = tokenArray[0]
+	} else if len(tokenArray) == 2 {
+		tokenString = tokenArray[1]
+	}
+
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		return jwtKey, nil
+	})
+
+	claims := token.Claims.(jwt.MapClaims)
+
+	if claims["role"] != "Admin" {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"message": "Unauthorized", "error": "You are not authorized to access this resource"})
+	}
+
+	err = c.Next()
+
+	return err
+}
