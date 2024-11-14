c41f9cefae5776798f30687e15805ce247f53b8c
atha3417 <atha.3417@gmail.com>
2024-11-14 08:42:58 +0700

    refactor: User model structure

--- models/user.go
+++ models/user.go
@@ -3,6 +3,5 @@ package models
 type User struct {
 	ID       uint   `json:"id" gorm:"primaryKey"`
 	Username string `json:"username" form:"username" validate:"required"`
-	Role     string `json:"role"`
-	Password string `json:"-" form:"password" validate:"required"`
+	Password string `json:"password" form:"password" validate:"required"`
 }
