2ef8c7bb555d918f52f8d366c4e6619b7e3be19d
atha3417 <atha.3417@gmail.com>
2024-11-16 00:46:40 +0700

    chore: remove unused function

--- repositories/user_repository.go
+++ repositories/user_repository.go
@@ -1,30 +1,9 @@
 package repositories
 
 import (
-	"github.com/google/uuid"
 	db "warung-informatika-be/database"
 	"warung-informatika-be/models"
 )
-
-func GetUsers() ([]models.User, error) {
-	var users []models.User
-
-	if err := db.DB.Find(&users).Error; err != nil {
-		return []models.User{}, err
-	}
-
-	return users, nil
-}
-
-func GetUser(uuid uuid.UUID) (models.User, error) {
-	var user models.User
-
-	if err := db.DB.First(&user, uuid).Error; err != nil {
-		return models.User{}, err
-	}
-
-	return user, nil
-}
 
 func GetUserByUsername(username string) (models.User, error) {
 	var user models.User
