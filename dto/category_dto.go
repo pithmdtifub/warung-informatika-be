17325e378c064ebe66a6fd3d66ff23049432f644
atha3417 <atha.3417@gmail.com>
2024-11-17 18:03:21 +0700

    feat: category, menu, user dto

---
+++ dto/category_dto.go
@@ -0,0 +1,11 @@
+package dto
+
+type CategoryRequest struct {
+	Name string `json:"name" validate:"required"`
+}
+
+type CategoryResponse struct {
+	ID    uint           `json:"id"`
+	Name  string         `json:"name"`
+	Menus []MenuResponse `json:"menus"`
+}
---
+++ dto/menu_dto.go
@@ -0,0 +1,19 @@
+package dto
+
+type MenuRequest struct {
+	Name        string  `json:"name" validate:"required"`
+	CategoryID  uint    `json:"category_id" validate:"required,number"`
+	Description string  `json:"description" validate:"required"`
+	Price       float64 `json:"price" validate:"required,number"`
+	Image       string  `json:"image" validate:"required,url"`
+}
+
+type MenuResponse struct {
+	ID           uint    `json:"id"`
+	Name         string  `json:"name"`
+	CategoryID   uint    `json:"category_id"`
+	CategoryName string  `json:"category"`
+	Description  string  `json:"description"`
+	Price        float64 `json:"price"`
+	Image        string  `json:"image"`
+}
