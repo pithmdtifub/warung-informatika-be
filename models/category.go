5496718bdb90d888aa377b5743d0a122bf4ea779
atha3417 <atha.3417@gmail.com>
2024-11-18 14:55:57 +0700

    chore: add constraint

--- models/category.go
+++ models/category.go
@@ -1,7 +1,14 @@
 package models
 
+import "gorm.io/gorm"
+
 type Category struct {
 	ID    uint   `gorm:"primaryKey"`
 	Name  string `gorm:"not null"`
-	Menus []Menu
+	Menus []Menu `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+}
+
+func (p *Category) BeforeFind(tx *gorm.DB) (err error) {
+	tx.Order("id ASC`")
+	return
 }
