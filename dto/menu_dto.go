5826f135fa7be9206bf290dc288b92ad05dc8373
atha3417 <atha.3417@gmail.com>
2024-11-18 16:32:34 +0700

    feat: add menu update dto

--- dto/menu_dto.go
+++ dto/menu_dto.go
@@ -8,6 +8,14 @@ type MenuRequest struct {
 	Image       string  `json:"image" validate:"required,url"`
 }
 
+type MenuUpdateRequest struct {
+	Name        string  `json:"name" validate:"required"`
+	CategoryID  uint    `json:"category_id" validate:"required,number"`
+	Description string  `json:"description" validate:"required"`
+	Price       float64 `json:"price" validate:"required,number"`
+	Image       string  `json:"image" validate:"required,url"`
+}
+
 type MenuResponse struct {
 	ID           uint    `json:"id"`
 	Name         string  `json:"name"`
