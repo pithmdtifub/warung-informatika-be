ec4a5637c26147d0e3b8430aecf2ed11c4e65165
atha3417 <atha.3417@gmail.com>
2024-11-22 08:10:55 +0700

    chore: change admin password

--- seeders/seeder.go
+++ seeders/seeder.go
@@ -5,6 +5,6 @@ import (
 )
 
 func Seed() {
-	UserSeeder(models.User{Username: "admin", Password: "akusayangpit123"})
+	UserSeeder(models.User{Username: "admin", Password: "akusayang(PIT):0"})
 	UserSeeder(models.User{Username: "user", Role: models.RoleUser, Password: "akusayangpit123"})
 }
