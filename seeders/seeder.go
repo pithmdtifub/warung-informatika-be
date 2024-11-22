6c0cce2206e6dc5897307ad06af38c7fa6ed1e93
atha3417 <atha.3417@gmail.com>
2024-11-22 08:10:18 +0700

    feat: user with user role access

--- seeders/seeder.go
+++ seeders/seeder.go
@@ -6,4 +6,5 @@ import (
 
 func Seed() {
 	UserSeeder(models.User{Username: "admin", Password: "akusayangpit123"})
+	UserSeeder(models.User{Username: "user", Role: models.RoleUser, Password: "akusayangpit123"})
 }
