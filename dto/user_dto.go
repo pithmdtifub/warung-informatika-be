c43c0bb4ad6e514a7e81fd996beec698f5786b3d
atha3417 <atha.3417@gmail.com>
2024-11-22 08:20:11 +0700

    fix: unique username validation

--- dto/user_dto.go
+++ dto/user_dto.go
@@ -2,5 +2,5 @@ package dto
 
 type UserRequest struct {
 	Username string `json:"username" validate:"required"`
-	Password string `json:"password" validate:"required"`
+	Password string `json:"password" validate:"required,min=8"`
 }
